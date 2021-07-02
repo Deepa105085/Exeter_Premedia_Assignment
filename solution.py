37b37c556c0816e9602b8c41843adfd180efc317
Deepa <deepakri2505@gmail.com>
2021-07-02 21:48:11 +0530

    "Deepa"

--- solution.py
+++ solution.py
@@ -4,11 +4,11 @@ start_time = time.time()
 def main():
     import pandas as pd
     header=['English','French']
-    df=pd.read_csv('../french_dictionary.csv', names=header)
+    df=pd.read_csv('french_dictionary.csv', names=header)
     data_dict = dict(zip(df.English, df.French))
     data_freq = dict.fromkeys(df.English, 0)
     eng=df['English']
-    file1 = open('../t8.shakespeare.txt', 'r')
+    file1 = open('t8.shakespeare.txt', 'r')
     text=file1.read()
     text=text.split(" ")
     transtext=""
@@ -27,7 +27,7 @@ def main():
          })
     filter_data= output_data[output_data.Frequency != 0]
     filter_data.to_csv(r'frequency.csv', index=False, header=True)
-    file2 = open('../t8.shakespeare.translated.txt', 'w')
+    file2 = open('t8.shakespeare.translated.txt', 'w')
     file2.write(transtext)
     file2.close()
 main()
